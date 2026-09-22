04f2735ae7a61619c6da18165d79e8c2137962aa
Eder Andrew <ederandrew0028@gmail.com>
2026-09-22 20:51:25 -0300

    criado o codigo do chamado

--- chamado.py
+++ chamado.py
@@ -1,2 +1,18 @@
 class Chamado:
-    pass
+    def __init__(self, id, solicitante, titulo, descricao):
+        self.id = id
+        self.solicitante = solicitante
+        self.titulo = titulo
+        self.descricao = descricao
+        self.status = 'ABERTO'
+
+    def alterar_status(self, novo_status):
+        self.status = novo_status
+
+    def exibir(self):
+        print("-" * 40)
+        print(f"ID: {self.id}")
+        print(f"Solicitante: {self.solicitante}")
+        print(f"Titulo: {self.titulo}")
+        print(f"Descrição: {self.descricao}")
+        print(f"Status: {self.status}")
